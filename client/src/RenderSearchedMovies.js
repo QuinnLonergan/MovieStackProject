6e2a64f40244b4f952d6c364b52c1fed7b68a474
Quinn Lonergan <qui99lo@gmail.com>
2022-01-06 13:22:44 -0500

    added imports

--- client/src/RenderSearchedMovies.js
+++ client/src/RenderSearchedMovies.js
@@ -65,37 +65,38 @@ export default function RenderSearchResults({ searchObject, cardstackId, rerende
     }
 
     if (searchObject.length > 0) {
-        return (
-            <div className='cardGroupSearch'>
-            <Grid container spacing={4}>
-                {movies ? (movies.map((card) => (
-              <Grid item key={card.id} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {card.Title}
-                    </Typography>
-                  </CardContent>
-                  <CardMedia
-                    component="img"
-                    image={card.Poster}
-                    alt={card.Poster}
-                  />
-                  <CardActions>
-                  {isAdded ? (
-                        <Button size="small" onClick={() => {handleAdd(card.Title, card.Poster)}}>Add to Stack</Button>
-                      ) : (
-                        <Button size="small" >Added</Button>
-                      )}
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))) : <h1></h1>}
-          </Grid>
-            </div>
-        )
+      <SearchedMovies />
+        // return (
+        //     <div className='cardGroupSearch'>
+        //     <Grid container spacing={4}>
+        //         {movies ? (movies.map((card) => (
+        //       <Grid item key={card.id} xs={12} sm={6} md={4}>
+        //         <Card
+        //           sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+        //         >
+        //         <CardContent sx={{ flexGrow: 1 }}>
+        //             <Typography gutterBottom variant="h5" component="h2">
+        //               {card.Title}
+        //             </Typography>
+        //           </CardContent>
+        //           <CardMedia
+        //             component="img"
+        //             image={card.Poster}
+        //             alt={card.Poster}
+        //           />
+        //           <CardActions>
+        //           {isAdded ? (
+        //                 <Button size="small" onClick={() => {handleAdd(card.Title, card.Poster)}}>Add to Stack</Button>
+        //               ) : (
+        //                 <Button size="small" >Added</Button>
+        //               )}
+        //           </CardActions>
+        //         </Card>
+        //       </Grid>
+        //     ))) : <h1></h1>}
+        //   </Grid>
+        //     </div>
+        // )
     } else return (
         <Grid sx={{
             marginTop: 10,
--- client/src/SearchedMovies.js
+++ client/src/SearchedMovies.js
@@ -1,3 +1,13 @@
+import { useEffect, useState } from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+
+
 export default function SearchedMovies({handleAdd, movies}){
 
 

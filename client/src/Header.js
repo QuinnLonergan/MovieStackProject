af7e9f8388251c38b2029892283c9eff21d0f5e2
Quinn Lonergan <qui99lo@gmail.com>
2022-01-13 11:13:01 -0500

    import state and effect

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
